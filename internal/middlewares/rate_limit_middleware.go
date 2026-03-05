627cc0b3f723b76f490e7a16817cd03df830a1d3
felipe1496 <felipe.oliveira1496@gmail.com>
2026-03-05 17:09:12 -0300

    feat: implements global and route level rate limit

---
+++ internal/middlewares/rate_limit_middleware.go
@@ -0,0 +1,46 @@
+package middlewares
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/felipe1496/open-wallet/internal/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+)
+
+func rateLimit(redisClient *redis.Client, maxRequests int, windowMilliseconds int, prefix string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ip := ctx.ClientIP()
+		key := fmt.Sprintf("rate_limit:%s:%s", prefix, ip)
+
+		val, err := redisClient.Incr(context.Background(), key).Result()
+		if err != nil {
+			ctx.Next()
+			return
+		}
+
+		if val == 1 {
+			redisClient.Expire(context.Background(), key, time.Duration(windowMilliseconds)*time.Millisecond)
+		}
+
+		if val > int64(maxRequests) {
+			apiErr := utils.NewHTTPError(http.StatusTooManyRequests, "You have exceeded the rate limit")
+			ctx.JSON(apiErr.StatusCode, apiErr)
+			ctx.Abort()
+			return
+		}
+
+		ctx.Next()
+	}
+}
+
+func GlobalRateLimitMiddleware(redisClient *redis.Client) gin.HandlerFunc {
+	return rateLimit(redisClient, utils.AppConfig.RateLimitMaxRequests, utils.AppConfig.RateLimitWindowMs, "global")
+}
+
+func RouteRateLimitMiddleware(redisClient *redis.Client, maxRequests int, windowMilliseconds int, prefix string) gin.HandlerFunc {
+	return rateLimit(redisClient, maxRequests, windowMilliseconds, prefix)
+}
--- internal/resources/auth/router.go
+++ internal/resources/auth/router.go
@@ -3,15 +3,17 @@ package auth
 import (
 	"log"
 
-	"github.com/felipe1496/open-wallet/db"
+	"github.com/redis/go-redis/v9"
 
+	"github.com/felipe1496/open-wallet/db"
+	"github.com/felipe1496/open-wallet/internal/middlewares"
 	"github.com/felipe1496/open-wallet/internal/services"
 	"github.com/felipe1496/open-wallet/internal/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Router(router *gin.Engine) {
+func Router(router *gin.Engine, redisClient *redis.Client) {
 	db, err := db.Conn(utils.AppConfig.DatabaseURL)
 	if err != nil {
 		log.Fatal(err)
@@ -20,6 +22,8 @@ func Router(router *gin.Engine) {
 	handler := NewHandler(db, services.NewGoogleService(), services.NewJWTService())
 	authGroup := router.Group("/api/v1/auth")
 	{
-		authGroup.POST("/login/google", handler.LoginGoogle)
+		authGroup.POST("/login/google",
+			middlewares.RouteRateLimitMiddleware(redisClient, 5, 300000, "POST:/api/v1/auth/login/google"),
+			handler.LoginGoogle)
 	}
 }
