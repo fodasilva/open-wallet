133cc6057b2a1ec124e17899db377a12b61f6dcc
felipe1496 <fodasilva@protonmail.com>
2026-04-25 12:04:18 -0300

    fix: required fields in generic response structs

--- internal/utils/querybuilder/types.go
+++ internal/utils/querybuilder/types.go
@@ -1,9 +1,9 @@
 package querybuilder
 
 type Metadata struct {
-	NextPage   bool `json:"next_page"`
-	Page       int  `json:"page"`
-	PerPage    int  `json:"per_page"`
-	TotalItems int  `json:"total_items"`
-	TotalPages int  `json:"total_pages"`
+	NextPage   bool `json:"next_page" binding:"required"`
+	Page       int  `json:"page" binding:"required"`
+	PerPage    int  `json:"per_page" binding:"required"`
+	TotalItems int  `json:"total_items" binding:"required"`
+	TotalPages int  `json:"total_pages" binding:"required"`
 }
