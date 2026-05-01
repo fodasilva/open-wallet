2bb081d71a7b96d918eb5b84af599039707ec9d2
felipe1496 <fodasilva@protonmail.com>
2026-05-01 13:37:08 -0300

    refactor: migrate routes from Gin to net/http

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,15 +1,15 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http"
 
 	"github.com/felipe1496/open-wallet/infra"
 	"github.com/felipe1496/open-wallet/internal/factory"
 )
 
-func SetupRoutes(r *gin.Engine, f *factory.Factory, cfg *infra.Config) {
-	SetupAuthRoutes(r, f, cfg)
-	SetupCategoriesRoutes(r, f, cfg)
-	SetupTransactionsRoutes(r, f, cfg)
-	SetupRecurrencesRoutes(r, f, cfg)
+func SetupRoutes(mux *http.ServeMux, f *factory.Factory, cfg *infra.Config) {
+	SetupAuthRoutes(mux, f, cfg)
+	SetupCategoriesRoutes(mux, f, cfg)
+	SetupTransactionsRoutes(mux, f, cfg)
+	SetupRecurrencesRoutes(mux, f, cfg)
 }
